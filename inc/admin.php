a514b5d58f827ad2b34057df115d4049b9435dec
Daria <daria.lukianchuk@gmail.com>
2021-04-06 10:46:38 +0300

    20210406 small fixes

--- inc/admin.php
+++ inc/admin.php
@@ -15,7 +15,8 @@ function cs__custom_menu_order( $menu_ord ) {
         'edit.php', // Posts
         'upload.php', // Media
         'wpcf7', // Contact
-        'theme-settings', // ACF: Theme Settings
+        //'theme-settings', // ACF: Theme Settings
+        'acf-options-general', // ACF: Theme Settings
         'separator2', // Second separator
         'themes.php', // Appearance
         'plugins.php', // Plugins
@@ -30,21 +31,21 @@ add_filter( 'custom_menu_order', 'cs__custom_menu_order', 10, 1 );
 add_filter( 'menu_order', 'cs__custom_menu_order', 10, 1 );
 
 
-
 /*** Disable content editor for specific pages ***/
 add_action( 'init', 'cs__hide_editor_for_pages' );
 function cs__hide_editor_for_pages() {
-    $post_id = $_GET['post'] ? $_GET['post'] : $_POST['post_ID'] ;
-    if( !isset( $post_id ) ) return;
- 
-    $template_file = get_post_meta($post_id, '_wp_page_template', true);
-
-    $templates_array = array(
-        'templates/tmpl-contacts.php',
-    );
+    if ( is_admin() ){
+        $post_id = $_GET['post'] ? $_GET['post'] : $_POST['post_ID'] ;
+        if( !isset( $post_id ) ) return;
     
-    // To disable for all pages: get_post_type($post_id)==='page'
-    if ( in_array($template_file, $templates_array) || $post_id==get_option('page_on_front') ){
-        remove_post_type_support('page', 'editor');
+        $template_file = get_post_meta($post_id, '_wp_page_template', true);
+
+        $templates_array = array(
+            'templates/tmpl-contacts.php',
+        );
+        
+        if ( in_array($template_file, $templates_array) || $post_id==get_option('page_on_front') ){
+            remove_post_type_support('page', 'editor');
+        }
     }
 }
